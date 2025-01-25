e8f9fe6747e353d30f4c1ec513fa54cf2dcbaaf9
Yoann Robert <luneroka@gmail.com>
2025-01-25 14:12:02 +0100

    Import quizzes selector into Quizzes

--- src/features/quizzes/Quizzes.js
+++ src/features/quizzes/Quizzes.js
@@ -1,22 +1,23 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import ROUTES from "../../app/routes";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import ROUTES from '../../app/routes';
 // import quiz selector
+import { selectQuizzes } from './quizzesSlice';
 
 export default function Quizzes() {
-  const quizzes = {}; // replace this with a call to your selector to get all the quizzes in state
+  const quizzes = useSelector(selectQuizzes);
   return (
-    <section className="center">
+    <section className='center'>
       <h1>Quizzes</h1>
-      <ul className="quizzes-list">
+      <ul className='quizzes-list'>
         {Object.values(quizzes).map((quiz) => (
           <Link key={quiz.id} to={ROUTES.quizRoute(quiz.id)}>
-            <li className="quiz">{quiz.name}</li>
+            <li className='quiz'>{quiz.name}</li>
           </Link>
         ))}
       </ul>
-      <Link to={ROUTES.newQuizRoute()} className="button">
+      <Link to={ROUTES.newQuizRoute()} className='button'>
         Create New Quiz
       </Link>
     </section>
