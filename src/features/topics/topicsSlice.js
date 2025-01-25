00185a6dd8d02f0d42bce96642ab0ee4920530b0
Yoann Robert <luneroka@gmail.com>
2025-01-25 12:28:45 +0100

    Add topicsSlice with addTopic reducer

---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const topicsSlice = createSlice({
+  name: 'topics',
+  initialState: {
+    topics: {},
+  },
+  reducers: {
+    addTopic: (state, action) => {
+      const { id, name, icon } = action.payload;
+      state.topics[id] = {
+        id: id,
+        name: name,
+        icon: icon,
+        quizIds: [],
+      };
+    },
+  },
+});
