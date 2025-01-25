1b519fee194fd3035930b0f88308d879f4d0268b
Yoann Robert <luneroka@gmail.com>
2025-01-25 13:08:29 +0100

    Import topicsSlice in Store and Topics

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 // import reducers
+import { topicsSlice } from '../features/topics/topicsSlice';
 
 export default configureStore({
   reducer: {
-    
+    topics: topicsSlice.reducer
   },
 });
